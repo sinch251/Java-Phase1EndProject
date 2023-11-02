533c41c06c289e0a5c9148540c1725b7d9d90b6c
Sinchana <mail2sinch@gmail.com>
2023-11-02 19:46:48 +0530

    phase1-end project code

---
+++ src/com/phaseOne/CameraRentalAppMain.java
@@ -0,0 +1,10 @@
+package com.phaseOne;
+
+public class CameraRentalAppMain {
+    public static void main(String[] args) { 
+    	
+    	
+    	CameraRentalApp app = new CameraRentalApp(); 
+    	app.login(); 
+    } 
+}
